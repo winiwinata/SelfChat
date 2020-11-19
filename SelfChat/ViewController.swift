45132d3704f8d9ca912e08acdb2545bced30b07a
wini <winiwinata90@gmail.com>
2020-11-19 10:40:32 +0800

    Initial Commit

---
+++ SelfChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfChat
+//
+//  Created by NDHU_CSIE on 2020/11/19.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
